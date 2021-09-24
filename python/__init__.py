1b7ea09dda24c7ddce6da408bea657aca68e83ec
infintropy <36432437+infintropy@users.noreply.github.com>
2021-09-24 16:48:45 -0400

    changed import strategy to be 2/3 compatible

--- python/__init__.py
+++ python/__init__.py
@@ -8,7 +8,7 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-from . import connection
-from . import util
-from . import widgets
-from . import sync
+from .connection import *
+from .util import *
+from .widgets import *
+from .sync import *
